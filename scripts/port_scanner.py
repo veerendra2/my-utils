de8c6458ada0262b0a5e7735ceee769805d28e23
Veerendra <veerendra2@users.noreply.github.com>
2017-03-03 17:53:07 +0530

    Localhost port scanner

---
+++ scripts/port_scanner.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+'''
+Author: Veerendra.K
+Description: nmap like utility, which gets open ports in localhost
+'''
+import socket
+import subprocess
+import sys
+import time
+from datetime import datetime
+t1=time.time()
+try:
+    for port in range(1,65535):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex(("127.0.0.1", port))
+	socket.setdefaulttimeout(0.01)
+        if result == 0:
+            print port
+except:
+	print "Socker Error!"
+t2=time.time()
+print "Delay=>",tin(t2-t1)
