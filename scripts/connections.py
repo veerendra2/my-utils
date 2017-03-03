84392e7cea3a893a23d8c2b2a7c2882e3789b755
Veerendra <veerendra2@users.noreply.github.com>
2017-03-03 17:51:06 +0530

    first commit

---
+++ scripts/connections.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+'''
+Author: Veerendra.K
+Description: Retrieves connection details of pid. Reads info from /proc/net/tcp/ /proc/[pid]/fd/ 
+'''
+import os
+import socket
+import struct
+import sys
+
+def parseTCPLine(line,pid):
+    if line:
+        part=line.split()
+        src_ip=socket.inet_ntoa(struct.pack("<L",int(part[1].split(":")[0],16)))
+        src_port=str(int(part[1].split(":")[1],16))
+        dst_ip=socket.inet_ntoa(struct.pack("<L",int(part[2].split(":")[0],16)))
+        dst_port=str(int(part[2].split(":")[1],16))
+        print pid.ljust(5," "),src_ip.ljust(14," "),src_port.ljust(15," "),dst_ip.ljust(16," "),dst_port
+        
+def getSockets(pid):
+    inodes=dict()
+    fd_path=os.path.join("/proc/",pid,"fd")
+    if os.path.exists(fd_path):
+        for fd in os.listdir(fd_path):
+            try:
+                f=os.readlink(os.path.join(fd_path,fd))
+            except:pass
+            if "socket:" in f:
+                inodes.setdefault(f.split("[")[1].split("]")[0], pid)
+    else:
+        print pid,"not found"
+    return inodes
+
+def readTCPFile(inodes):
+    with open("/proc/net/tcp") as f:
+        for line in f.readlines():
+            for node,pid in inodes.items():
+                if node in line:
+                    parseTCPLine(line,pid)
+
+def header():
+    print "PID".ljust(5," "),"Source IP".ljust(13," "),"Source Port".ljust(15," "),"Destination IP".ljust(15," "),"Destination Port"
+    print "".ljust(65,"-")
+
+if __name__=="__main__":
+    if len(sys.argv)>1:
+        header()
+        for pid in sys.argv[1:]:
+            if pid.isdigit():
+                readTCPFile(getSockets(pid))
+    else:
+        header()
+        for dir in os.listdir("/proc"):
+            if dir.isdigit():
+                readTCPFile(getSockets(dir))
