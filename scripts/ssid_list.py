ca296fcca83966a5361103ad3da3b0eb4944ee39
Veerendra <veerendra2@users.noreply.github.com>
2017-06-08 22:24:46 +0530

    Create ssid_list.py

---
+++ scripts/ssid_list.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+"""
+Author: Veerendra. Kakumanu
+Description: Displays near wifi hotspots
+"""
+import subprocess
+import os
+import json
+import re
+import argparse
+
+re_compile_patterns1={"MAC Address" : 'Address:(.*)',
+                    "Channel"       : 'Channel:(.*)',
+                    "Frequency"     : 'Frequency:(.\S+ .\S+)',
+                    "Encryption"    : 'Encryption key:(.*)',
+                    "Mode"          : 'Mode:(.*)',
+                    "Quality"       : 'Quality=(.\S+)',
+                    "ESSID"         : 'ESSID:(.*)',
+                    "Singal Level"  : 'Signal level=(.*)',
+                    "ID"            : '(.*) - Address'}
+
+re_compile_patterns2={"Encryption Algorithm WPA" : 'IE: WPA(.*)',
+                    "Encryption Algorithm"       : 'IE: IEEE(.*)',
+                    "Group Cipher"               : 'Group Cipher :(.*)',
+                    "Pairwise Ciphers"           : 'Pairwise Ciphers(.*)',
+                    "Authentication Suites"      : 'Authentication Suites(.*)'}
+
+def get_ap_results():
+    result=None
+    ap=dict()
+    for x in range(3):
+        if x==2:
+            print "iwlist is not working?"
+            exit(1)
+        try:
+            return subprocess.check_output("iwlist wlan0 s",shell=True)
+            break
+        except:
+            continue
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Displays wifi hotspots info near to you. [Coded by VEERENDRA KAKUMANU]')
+    parser.add_argument('-j', action='store_true', dest='json_val', default=False ,help='Dumps in json format')
+    parser.add_argument('-v', action='version', version='%(prog)s 1.0')
+    results=parser.parse_args()
+    ssids=get_ap_results()
+    for name,pattern in re_compile_patterns1.items():
+        re_compile_patterns1[name]=re.compile(pattern)
+    for names,pattern in re_compile_patterns2.items():
+        re_compile_patterns2[names]=re.compile(pattern)
+    final_json=list()
+    if not results.json_val:
+        print "+".ljust(5,"-")+"+".ljust(22,"-")+"+".ljust(20,"-")+"+".ljust(10,"-")+"+".ljust(12,"-")+"+".ljust(15,"-")+"+"
+        print "| ID".ljust(5," ")+"|"+"        ESSID        |"+"    MAC Address    |"+" Channel |"+" Frequency |"+" Singal Level |"
+        print "+".ljust(5,"-")+"+".ljust(22,"-")+"+".ljust(20,"-")+"+".ljust(10,"-")+"+".ljust(12,"-")+"+".ljust(15,"-")+"+"
+    for line in ssids.split("Cell"):
+        if line and "Scan completed" not in line:
+            wifi_json=dict()
+            for names,objects in re_compile_patterns1.items():
+                wifi_json.setdefault(names,objects.findall(line)[0].strip().strip('"'))
+            if not results.json_val:
+                print "|",wifi_json["ID"].ljust(3," ")+"|",wifi_json["ESSID"].ljust(20," ")+"|",wifi_json["MAC Address"].ljust(18," ")+"|",wifi_json["Channel"].ljust(8," ")+"|",wifi_json["Frequency"].ljust(10," ")+"|",wifi_json["Singal Level"].ljust(13," ")+"|"
+            for names,objects in re_compile_patterns2.items():
+                try:
+                    wifi_json.setdefault(names,objects.findall(line)[0].strip())
+                except: pass
+            final_json.append(wifi_json)
+    if not results.json_val:
+        print "+".ljust(5,"-")+"+".ljust(22,"-")+"+".ljust(20,"-")+"+".ljust(10,"-")+"+".ljust(12,"-")+"+".ljust(15,"-")+"+"
+    else:
+        print json.dumps(final_json)
