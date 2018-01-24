0f05e0ce7d7bf9ea29fa95fdbb34449b3b53025c
Veerendra <veerendrav2@gmail.com>
2018-01-24 23:00:01 +0530

    Create set_jekyll.sh

---
+++ scripts/set_jekyll.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Automated script to install Ruby2.4, Jekyll and its gems in Ubuntu 14.04
+
+sudo apt-add-repository ppa:brightbox/ruby-ng -y
+sudo apt-get update
+sudo apt-get install make g++ ruby2.4 ruby2.4-dev -y
+sudo gem install jekyll bundler
+echo "** Installing Jekyll related GEMs **"
+sudo gem install jekyll-sitemap
+sudo gem install jekyll-feed
+sudo gem install jekyll-assets
+sudo gem install jekyll-redirect-from
+sudo gem install jekyll-paginate
